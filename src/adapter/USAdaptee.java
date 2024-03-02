9dd824c6de3f0dc04b36580c856edceb79069d0a
Akshat Pandey <akshatpandey@Akshats-MacBook-Air-3.local>
2024-03-01 19:07:32 -0500

    Cleaned Adapter files

--- src/adapter/USAdaptee.java
+++ src/adapter/USAdaptee.java
@@ -25,34 +25,4 @@ public class USAdaptee implements UniversalAdaptee {
         System.out.println("Here's the output voltage you need!");
     }
 }
-/*
-package adapter;
 
-public class IndianPinClient {
-
-    CurrentConfiguration currentConfiguration;
-
-    Target target;
-    //in real word this could come from upstream
-    IndianPinClient(CurrentConfiguration currentConfiguration){
-        this.currentConfiguration = currentConfiguration;
-    }
-
-    public CurrentConfiguration getCurrentConfiguration() {
-        return currentConfiguration;
-    }
-
-    public void setCurrentConfiguration(CurrentConfiguration currentConfiguration) {
-        this.currentConfiguration = currentConfiguration;
-    }
-
-    public Target getTarget() {
-        return target;
-    }
-
-    public void setTarget(Target target) {
-        this.target = target;
-    }
-}
-
- */
